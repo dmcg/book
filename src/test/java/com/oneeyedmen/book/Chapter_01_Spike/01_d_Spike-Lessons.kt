c4c202fbd2eac9bb98079a57de1ccdd91d20086c
Duncan McGregor <duncan@oneeyedmen.com>
2017-02-25 21:41:57 +0000

    Move conclusions into a final section of Chapter 1

---
+++ src/test/java/com/oneeyedmen/book/Chapter_01_Spike/01_d_Spike-Lessons.kt
@@ -0,0 +1,28 @@
+package com.oneeyedmen.book.Chapter_01_Spike
+
+/*-
+Conclusions
+-----------
+
+What has this chapter been trying to prove?
+
+Firstly I've been trying to prove to myself that this model works for me when actually explaining concepts. Unless
+ you're viewing the source version of this text, you won't see that there is a subtlety around the different versions
+ of the code existing in the same source file. When explaining code on my blog I would have to move forward and back
+ between different source versions, cutting and pasting them into the code. Here, I can keep the different versions
+ in the same file, which is a big productivity win.
+
+Secondly, I'm trying to demonstrate risk reduction - making sure that I don't commit too much effort into a doomed
+ venture. We've done just enough here to show that the book-in-comments approach has legs. No doubt I'll spend days
+ on tooling to produce the book before I'm finished - the code is rough and the inability to write about my own notation
+ without invoking it is a shame - but right now we have confidence without too much cost.
+
+Tooling is another take-away. Modern developers will go to great lengths to increase their own productivity by building
+ tools to leverage their time. Sometimes these are simple scripts, sometimes full applications. I'm hoping to leverage
+ the power of an IDE that I already know with some text processing to produce something greater than the sum of its
+ parts.
+
+Leverage is my final thought. Great developers find ways of solving several problems at once. I don't claim to be a
+  great developer, but I am secretly pleased with having tested the writing model, written some tooling to support it,
+  and perhaps demonstrated some aspects of modern programming, all at the same time.
+-*/
