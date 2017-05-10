acd136b316972215882af57fba6c498ac4f570b6
Duncan McGregor <duncan@oneeyedmen.com>
2017-05-10 23:05:05 +0100

    Start work on an introduction

---
+++ src/test/java/com/oneeyedmen/book/Chapter_00_Introduction/00_a_Introduction.kt
@@ -0,0 +1,19 @@
+package com.oneeyedmen.book.Chapter_00_Introduction
+
+/*-
+This is not a book to teach you programming. It assumes that you have already mastered the basics, consider yourself an
+ accomplished developer, but want to improve. Frankly I’m in the same boat. My aim is to write the book that I wished
+ I’d read 5 years ago, one that discusses software development in the way that we might if we bumped into each other at
+ a conference, or paired on some code for a client.
+
+I’ve been lucky enough to work with some excellent developers over the years, largely in a community that coalesced
+ around eXtreme Programming in London around 2000. We explored the effects of pair programming, Test Driven Development
+ and refactoring together, and reported our progress at conferences and workshops. In the early days our code would
+ have been characterised as Object Oriented, but these days there is a trend towards a mixed OO / functional style.
+
+built up around and whilst it has struck me how much we agree
+I try to be prescriptive only when
+
+-*/
+
+
