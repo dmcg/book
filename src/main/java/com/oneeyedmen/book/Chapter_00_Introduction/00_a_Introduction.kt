278b571abcdd2947eb9ffafe13142eff9d3f8c09
Duncan McGregor <duncan@oneeyedmen.com>
2017-06-06 09:20:32 +0100

    More Introduction

--- src/main/java/com/oneeyedmen/book/Chapter_00_Introduction/00_a_Introduction.kt
+++ src/main/java/com/oneeyedmen/book/Chapter_00_Introduction/00_a_Introduction.kt
@@ -5,7 +5,9 @@ package com.oneeyedmen.book.Chapter_00_Introduction
 
 The aim of this book is to teach software development by example. Rather than canned examples with predetermined teaching goals and known solutions, it documents programming episodes, with real-world uncertainties, blind-alleys, short-cuts and failures. It is, in short, a book about engineering.
 
+As far as I can muster the energy to do so, I have tried to document things as they actually happened. There are times when I have expurgated some tedious details in order to keep you reading, but I have not returned to an example to paint it in a better, or worse, light than live. Post-production has been limited to formatting, copy-editing and conclusion-drawing;  no scenes were filmed out of order or staged for entertainment value.
 
+To keep me honest, you can see the history of this book in its [GitHub repository](https://github.com/dmcg/episodes). That would also be a great place to submit any corrections, suggestions or feedback.
 
 -*/
 
